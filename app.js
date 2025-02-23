99395953d8429b9011dd110b57916486ef1eba7e
sheri6186 <sheri.6186@gmail.com>
2025-02-24 01:32:19 +0500

    basic calculator DMAS

---
+++ app.js
@@ -0,0 +1,32 @@
+function add(num1,num2){
+    return num1+num2;
+}
+
+function subtraction(num1,num2){
+    return num1-num2;
+}
+
+function multiplication(num1,num2){
+    return num1*num2;
+}
+
+function division(num1,num2){
+    return num1/num2;
+}
+function calculate(a,b,opretor){
+ if(opretor==add){
+    return add(a,b);
+ }else if(opretor==subtraction){
+    return subtraction(a,b);
+ }else if(opretor==multiplication){
+    return multiplication(a,b);
+ }else if(opretor==division){
+    return division(a,b);
+ }else if(!opretor){
+    return "please give funtion operator";
+ }else{
+    return "something is missing";
+ }
+}
+console.log(calculate(100));
+
