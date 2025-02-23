d3a0b8ebe8cba141f2c45bbb98c3842fac2b28bf
sheri6186 <sheri.6186@gmail.com>
2025-02-24 01:33:09 +0500

    basic DMAS calculator mathemetical using basic methametical operation

---
+++ weather.js
@@ -0,0 +1,10 @@
+let temp=-6
+if(temp<=-10){
+    console.log("kill yourself");
+}else if(temp>-10 || temp<-5){
+    console.log("super cold");
+}else if(temp<0||temp<=10){
+    console.log("cold");
+}else if(temp<-10){
+    console.log("kill yourself");
+}
