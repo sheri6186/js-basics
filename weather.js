9d4daa5f89d3a32fa47dea1039e6633033818e89
sheri6186 <sheri.6186@gmail.com>
2025-02-28 15:05:03 +0500

    weather forcast absolute basics logic

--- weather.js
+++ weather.js
@@ -1,10 +1,12 @@
-let temp=-6
-if(temp<=-10){
-    console.log("kill yourself");
-}else if(temp>-10 || temp<-5){
+
+let temp=20
+if(temp<=0 || temp>30){
+    console.log("weather is bad");
+}else if(temp>=-5|| temp>=-10){
     console.log("super cold");
-}else if(temp<0||temp<=10){
-    console.log("cold");
+
 }else if(temp<-10){
     console.log("kill yourself");
+}else{
+    console.log("weather is good")
 }
