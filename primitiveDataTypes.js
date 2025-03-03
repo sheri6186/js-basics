ffd5aa814a23ad2e14d9da76a0054c6de3427791
sheri6186 <sheri.6186@gmail.com>
2025-03-03 14:20:25 +0500

    primitive data types of javascript

---
+++ primitiveDataTypes.js
@@ -0,0 +1,11 @@
+// primitive data types: number, string, null, undefind, boolean, bigInt and Symbol
+
+const value = 159; //number
+let name="asrar";//string
+const user=null;//empty null 
+const password=undefined;//undefind
+let email;//define
+const id=Symbol('123');//symbol
+const idAnother=Symbol('123');//symbol
+console.log(id===idAnother);
+const bigNumber=12343365464987998798798897987564465462n;//bigInt
