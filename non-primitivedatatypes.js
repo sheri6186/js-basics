01873c6e3ae7c786fe6e766f85ae9406c397533a
sheri6186 <sheri.6186@gmail.com>
2025-03-03 21:47:54 +0500

    non-primitive data types in js

---
+++ non-primitivedatatypes.js
@@ -0,0 +1,14 @@
+// non-primitive datatypes (referrence) function, arrays, objects
+
+// object
+const myobj={
+    name:"sheri",
+    age:26
+}
+// function
+const user=function(){
+    console.log("user:"+myobj.name);
+}
+
+// array
+const movies=["the great gatsby","sing2","man from torronto"];
