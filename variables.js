33c9556735a067cfad276c695f9d95ebdf4aef7a
sheri6186 <sheri.6186@gmail.com>
2025-03-07 12:59:02 +0500

    variable explained

---
+++ variables.js
@@ -0,0 +1,7 @@
+let userName="sheri";//username defined
+var accountId=20; //account id defined
+const password="21212121";//password will not update
+userName="yousaf"; //username will update
+accountId=50;
+console.table([userName,accountId,password]);
+
