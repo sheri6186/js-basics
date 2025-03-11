9a092204ea40af0abed82935e2e11f39603d78e2
sheri6186 <sheri.6186@gmail.com>
2025-03-11 14:29:01 +0500

    investigation of datatypes convertion

---
+++ datatypesconversion.js
@@ -0,0 +1,11 @@
+// let number=33;
+// console.log(typeof number)
+// let variable=String(number)
+// console.log(variable)
+// console.log(typeof variable)
+
+
+const boolean =true
+let convertBoolean= Number(boolean)
+console.log(boolean)
+console.log(typeof convertBoolean)
