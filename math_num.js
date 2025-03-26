3f63e524df92a14b108eb6ded6997b0f902cf5e4
sheri6186 <sheri.6186@gmail.com>
2025-03-27 00:30:28 +0500

    math object and its properties explained

---
+++ math_num.js
@@ -0,0 +1,23 @@
+const age=new Number(25);
+// console.log(age.toString());
+// console.log(typeof age);
+
+
+
+// console.log(Math)
+// console.log(Math.PI)
+
+// console.log(Math.abs(-10));
+// console.log(Math.ceil(10.1));
+// console.log(Math.floor(10.9));
+// console.log(Math.round(10.5));
+// console.log(Math.max(10,20,30,40,50));  
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10+1));
+
+
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
