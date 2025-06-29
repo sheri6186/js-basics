1b4519faa455bdaeef6658cb6d1808c0bf1fc2ca
sheri6186 <sheri.6186@gmail.com>
2025-06-29 20:12:26 +0500

    refactor code

--- DOM_ASSET/script.js
+++ DOM_ASSET/script.js
@@ -1,24 +1,22 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.body;
 
-buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        if (event.target.id === "grey") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "white") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "blue") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "yellow") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "green") {
-            body.style.backgroundColor = event.target.id;
-            console.log("hello")
-        }
-         
-    });
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "green") {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
 });
