81e59a6ccbd469ba27b36b57e59245bc9ad1776a
sheri6186 <sheri.6186@gmail.com>
2025-07-02 15:15:48 +0500

    basic projects learning for javascript

---
+++ DOM_ASSET/BMI-Genrator/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please enter a valid height number ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `please enter a valid weight number${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi <= 18.6) {
+      results.innerHTML = `<span>under weight:${bmi}</span>`;
+    } else if (bmi <= 18.6 && 24.9) {
+      results.innerHTML = `<span>normal range:${bmi}</span>`;
+    } else {
+      results.innerHTML = `<span>overweight:${bmi}</span>`;
+    }
+  }
+});
