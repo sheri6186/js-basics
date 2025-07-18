8346b10620f5b7b0328cb9b6793b1ba97fee9299
sheri6186 <sheri.6186@gmail.com>
2025-07-18 16:15:22 +0500

    refactor

--- DOM_ASSET/BMI-Genrator/script.js
+++ DOM_ASSET/BMI-Genrator/script.js
@@ -2,6 +2,7 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   const height = parseInt(document.querySelector('#height').value);
+  
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
   if (height === '' || height < 0 || isNaN(height)) {
