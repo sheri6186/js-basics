488277e53b4f9a0f3afbe1f4b429c6c3bc2479c3
Asrar Ul Haq <86061538+sheri6186@users.noreply.github.com>
2025-10-15 15:54:21 +0500

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,28 +1,43 @@
-const promiseone=new Promise((resolve,reject)=>{
+const promiseone = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log("promise one is resolved")
-        
+
     }, 1000);
     resolve("promise one is resolved")
 })
-promiseone.then(()=>{
+promiseone.then(() => {
     console.log("promise one is comepleted")
 })
-const promiseTwo=new Promise((resolve,reject)=>{
+const promiseTwo = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log("promise two is resolved")
         resolve("promise two is resolved")
     }, 1000);
 })
-promiseTwo.then(()=>{console.log("promise two is comepleted")})
+promiseTwo.then(() => { console.log("promise two is comepleted") })
 
 
-const promiseThree=new Promise((resolve,reject)=>{
-    resolve({username:"sheri",email:"sheri@gmail.com"})
+const promiseThree = new Promise((resolve, reject) => {
+    resolve({ username: "sheri", email: "sheri@gmail.com" })
 
-   
+
 })
-promiseThree.then((user)=>{
+promiseThree.then((user) => {
     console.log(user)
 })
 
+
+const promiseFour = new Promise((resolve, reject) => {
+    let error = false;
+    if (!error) {
+        resolve({ username: "sheri", password: "123" })
+
+    } else {
+        reject("something went wrong")
+    }
+})
+promiseFour
+.then((user) => { return user.username })
+.then((username) => console.log(username))
+.catch((error) => console.log(error))
+.finally(() => console.log("the promise is either resolved or rejected"))
